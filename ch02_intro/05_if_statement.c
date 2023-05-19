0c6c354346fe840a04a7459e183efe3cf36f9c26
liangjinyong <liangjy881@gmail.com>
2023-05-19 11:13:32 +0900

    ch02 finished

---
+++ ch02_intro/05_if_statement.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Enter two integers, and I will tell you\n");
+    printf("the relationships they satisfy: ");
+
+    int number1 = 0; // first number to be read from user
+    int number2 = 0; // second number to be read from user
+
+    scanf("%d %d", &number1, &number2); // read two integers
+
+    if (number1 == number2)
+    {
+        printf("%d is equal to %d\n", number1, number2);
+    }
+
+    if (number1 != number2)
+    {
+        printf("%d is not equal to %d\n", number1, number2);
+    }
+
+    if (number1 < number2)
+    {
+        printf("%d is less than %d\n", number1, number2);
+    }
+
+    if (number1 > number2)
+    {
+        printf("%d is greater than %d\n", number1, number2);
+    }
+
+    if (number1 <= number2)
+    {
+        printf("%d is less than or equal to %d\n", number1, number2);
+    }
+
+    if (number1 >= number2)
+    {
+        printf("%d is greater than or equal to %d\n", number1, number2);
+    }
+}
