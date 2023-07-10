fdeeec1bfdb77f7d1e4d5a43541ef5fa919ecb31
liangjinyong <liangjy881@gmail.com>
2023-07-10 18:30:32 +0900

    to: bubble sort

---
+++ ch06_arrays/10_array_address.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char array[5] = ""; // define an array of size 5
+
+    printf("    array = %p\n&array[0] = %p\n   &array = %p\n", array, &array[0], &array);
+}
