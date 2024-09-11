a5ecedcc83568b10612a3f88258e3ea4cc2748ab
liangjinyong <liangjy881@gmail.com>
2024-09-11 23:07:10 +0900

    7.4 passing arguments to functions by reference

---
+++ ch07_pointers/03_pass_by_reference.c
@@ -0,0 +1,21 @@
+// fig07_03.c
+// Cube a variable using pass-by-reference with a pointer argument.
+
+#include <stdio.h>
+
+void cubeByReference(int *nPtr); // function prototype
+
+int main(void)
+{
+    int number = 5; // initialize number
+
+    printf("The original value of number is %d", number);
+    cubeByReference(&number);
+    printf("\nThe new value of number is %d\n", number);
+}
+
+// calculate cube of *nPtr; actually modifies number in main
+void cubeByReference(int *nPtr)
+{
+    *nPtr = *nPtr * *nPtr * *nPtr;
+}
